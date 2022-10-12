7c426e6642054420e6cd0e003169c3d41992c418
nonfermata <nonfermata@gmail.com>
2022-10-12 20:49:54 +0600

    layouts & userPage

--- src/app/components/loading.jsx
+++ src/app/components/loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
     return (
-        <div className="loading">
+        <div className="loading m-4">
             L O A D I N G . . . . .
         </div>
     );
